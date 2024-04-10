d595f1f94682db056cba79a8020752da7b0e39ef
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2024-04-11 00:22:40 +0330

    add some compoenets

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -1,43 +1,14 @@
 "use server";
-import { z } from "zod";
-import { db } from "@/lib/db";
-import { revalidatePath } from "next/cache";
 import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { InputType, ReturnType } from "./types";
 
-export type State = {
-  errors?: {
-    title?: string[];
-  };
-  message?: string | null;
-};
-const CreateBoard = z.object({
-  title: z.string().min(3, {
-    message: "Mininmum length of 3 letters is required"
-  })
-});
-
-export async function create(prevState: State, formData: FormData) {
-  const { orgId } = auth();
-  const validateFields = CreateBoard.safeParse({
-    title: formData.get("title")
-  });
-  if (!validateFields.success) {
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId } = auth();
+  if (!userId) {
     return {
-      errors: validateFields.error.flatten().fieldErrors,
-      message: "Missing Fields."
+      error: "Unauthorized"
     };
   }
-  const { title } = validateFields.data;
-  try {
-    await db.board.create({
-      data: {
-        title
-      }
-    });
-  } catch (error) {
-    return { message: "Database Error" };
-  }
-  revalidatePath(`/organization/${orgId}`);
-  redirect(`/organization/${orgId}`);
-}
+  const {title} = data;
+  
+};
